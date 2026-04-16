Fix ProGen2 training precision semantics

src/progen2/modeling/wrapper.py
@@ -31,12 +31,14 @@ class OfficialProGen2CausalLM:
         tokenizer_path,
         checkpoint_subdir=None,
         device='cpu',
-        use_fp16=True,
+        use_fp16=False,
+        autocast_dtype=None,
     ):
         self.device = torch.device(device)
         self.official_code_dir = str(official_code_dir)
         self.checkpoint_dir = resolve_checkpoint_load_dir(checkpoint_dir, checkpoint_subdir)
         self.tokenizer = OfficialProGen2Tokenizer(tokenizer_path)
+        self.autocast_dtype = autocast_dtype
 
         model_cls = get_progen_model_class(self.official_code_dir)
         load_kwargs = {'low_cpu_mem_usage': True}
@@ -55,9 +57,9 @@ class OfficialProGen2CausalLM:
 
     @property
     def autocast_context(self):
-        if self.device.type != 'cuda':
+        if self.device.type != 'cuda' or self.autocast_dtype is None:
             return nullcontext()
-        return torch.autocast(device_type='cuda', dtype=torch.float16)
+        return torch.autocast(device_type='cuda', dtype=self.autocast_dtype)
 
     def train(self):
         self._root_model().train()

src/progen2/rl/trainer.py
@@ -157,6 +157,7 @@ class ProGen2SGRPOTrainer:
     def __init__(self, config, output_dir):
         self.config = config
         self.output_dir = output_dir
+        self.autocast_dtype = torch.bfloat16 if config.bf16 else None
         self.accelerator = Accelerator(
             step_scheduler_with_optimizer=False,
             gradient_accumulation_steps=config.gradient_accumulation_steps,
@@ -181,7 +182,8 @@ class ProGen2SGRPOTrainer:
                 tokenizer_path=config.tokenizer_path,
                 checkpoint_subdir=config.checkpoint_subdir,
                 device=self.device,
-                use_fp16=not config.bf16,
+                use_fp16=False,
+                autocast_dtype=self.autocast_dtype,
             ),
             trainable=True,
         )
@@ -192,7 +194,8 @@ class ProGen2SGRPOTrainer:
                 tokenizer_path=config.tokenizer_path,
                 checkpoint_subdir=config.checkpoint_subdir,
                 device=self.device,
-                use_fp16=not config.bf16,
+                use_fp16=False,
+                autocast_dtype=self.autocast_dtype,
             ),
             trainable=False,
         )